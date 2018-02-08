ae78fea319f225dab55a8faa04a9894955e6ce03
Ntsoane <36257564+Ntsoane@users.noreply.github.com>
2018-02-08 10:59:34 +0200

    This is my first site and it has my nice pictures
    This is my first web-site I ever uploaded.I built it in three days from scratch using HTML, CSS  and Java Script and it is very attractive,nice,trust me you will like it.ENJOY!

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/petrus.jpg') {
+      myImage.setAttribute ('src','images/petrus1.jpg');
+    } else {
+      myImage.setAttribute ('src','images/petrus.jpg');
+    }
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+//User name code
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to my site, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome back, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
